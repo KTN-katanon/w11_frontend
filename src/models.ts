588adc2a476b19d13022532a7a11680f3dc38b50
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-02-25 00:31:37 +0700

    Fix Product Id

--- src/models.ts
+++ src/models.ts
@@ -1,15 +1,17 @@
 export interface Type {
-  id?: number;
-  name: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-  deletedAt?: Date;
+  id?: number
+  name: string
+  createdAt?: Date
+  updatedAt?: Date
+  deletedAt?: Date
 }
 export interface Product {
-  id: number
+  id?: number
   name: string
-  category: 'drink' | 'bakery'
+  typeId: number
   price: number
+  type?: Type
+  imageUrl: string
 }
 
 export interface User {
